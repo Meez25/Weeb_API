4f2a64810e3e87cc86c0e22ddb8d9c378d73218d
Yann Bronner <yann.bronner@gmail.com>
2025-11-03 15:42:57 +0100

    Implement endpoint to register contact

---
+++ weebapi/contact/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email_address",
+                    "phone_number", "message", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("first_name", "last_name",
+                     "email_address", "phone_number", "message")
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at",)
