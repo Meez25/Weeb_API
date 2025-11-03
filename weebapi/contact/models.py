fd347e1d513611a69f464582570b0fb3aa949f98
Yann Bronner <yann.bronner@gmail.com>
2025-11-04 00:22:57 +0100

    automatic satisfaction review

--- weebapi/contact/models.py
+++ weebapi/contact/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from satisfaction.satisfaction import analyze_satisfaction
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=30)
@@ -9,3 +11,9 @@ class Contact(models.Model):
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    satisfaction = models.IntegerField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.message:
+            self.satisfaction = analyze_satisfaction(self.message)
+        super().save(*args, **kwargs)
